6f543029871558b4c672830aed81eb1a22c6a78a
G-Kapish <169173656+G-Kapish@users.noreply.github.com>
2024-07-18 23:57:26 +0530

    d1nd2

---
+++ index.js
@@ -0,0 +1,45 @@
+// var state = 
+// {
+//     taskList:
+//     [
+//         {
+//             imageUrl:"",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         }
+//     ]
+// }
+
+    var state = 
+    {
+        taskList: []
+    };
+
+    // DOM Operations
+
+    var taskContents = document.querySelector(".task__contents")
+    var taskModal = document.querySelector(".task__modal__body")
+    
+    console.log(taskModal)    
+    console.log(taskContents)
+
+    
